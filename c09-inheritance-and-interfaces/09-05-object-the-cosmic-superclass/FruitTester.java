b30fa5c2364bd21138d79fa0d70a0cf41514eabd
leehpham <lehoangpham1092@gmail.com>
2018-03-28 08:47:56 -0500

    ...

--- c09-inheritance-and-interfaces/09-05-object-the-cosmic-superclass/FruitTester.java
+++ c09-inheritance-and-interfaces/09-05-object-the-cosmic-superclass/FruitTester.java
@@ -11,6 +11,14 @@ public class FruitTester
         System.out.println(obj instanceof Fruit); // -> true
 
         // But you still can't use it to call getColor
-        
+        // obj.getColor();
+
+        // If you want to do so, you need to cast it to Fruit and assign its value to a new reference
+        // NOTE: for some reason you can't reassign it to itself
+        // obj = (Fruit) obj;
+        // System.out.println(obj.getColor());
+
+        Fruit temp = (Fruit) obj;
+        System.out.println(temp.getColor()); // -> red
     }
 }
