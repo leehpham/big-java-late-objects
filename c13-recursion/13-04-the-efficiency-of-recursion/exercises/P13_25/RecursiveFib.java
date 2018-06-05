4305889e4f6a6d3e56c7893a2ba18acbe5e311d2
leehpham <lehoangpham1092@gmail.com>
2018-06-04 21:33:44 -0500

    ...

--- c13-recursion/13-04-the-efficiency-of-recursion/exercises/P13_25/RecursiveFib.java
+++ c13-recursion/13-04-the-efficiency-of-recursion/exercises/P13_25/RecursiveFib.java
@@ -1,29 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RecursiveFib
 {
     public static void main(String[] args)
     {
+        ArrayList<Long> computedValues = new ArrayList<Long>();
+        computedValues.add(0L);
         Scanner in = new Scanner(System.in);
         System.out.print("Enter n: ");
         int n = in.nextInt();
 
-        long f = fib(n);
-
-        System.out.println("fib(" + n + ") = " + f);
+        for (int i = 1; i <= n; i++)
+        {
+            System.out.printf("fib(%d) = %d\n", i, fib(i, computedValues));
+        }
         in.close();
     }
 
-    public static long fib(int n)
+    public static long fib(int n, ArrayList<Long> computedValues)
     {
-        System.out.println("Entering fib: n = " + n);
-        long f;
-        if (n <= 2) { f = 1; }
+        if (n < computedValues.size())
+        {
+            return computedValues.get(n);
+        }
+        if (n <= 2)
+        {
+            computedValues.add(1L);
+            return 1;
+        }
         else
         {
-            f = fib(n - 1) + fib(n - 2);
+            long f = fib(n - 1, computedValues) + fib(n - 2, computedValues);
+            computedValues.add(f);
+            return f;
         }
-        System.out.println("Exiting fib: n = " + n + " return value = " + f);
-        return f;
     }
 }
