660b2d5e7c3b3621fef979ff4404cd917f0ed750
Hoang Pham <lehoangpham1092@gmail.com>
2019-09-07 22:49:25 -0500

    ...

--- c09-inheritance-and-interfaces/business/P09_21/Appointment.java
+++ c09-inheritance-and-interfaces/business/P09_21/Appointment.java
@@ -1,20 +1,36 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 public class Appointment
 {
     private String description;
-    private int year;
-    private int month;
-    private int day;
+    private Date date;
 
-    public Appointment(String description, int year, int month, int day)
+    public Appointment(String description, int month, int day, int year) throws ParseException
+    {
+        this.setDescription(description);
+        this.setDate(month, day, year);
+    }
+
+    public void setDescription(String description)
     {
         this.description = description;
-        this.year = year;
-        this.month = month;
-        this.day = day;
     }
 
     public String getDescription()
     {
-        return description;
+        return this.description;
+    }
+
+    public void setDate(int month, int day, int year) throws ParseException
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        this.date = simpleDateFormat.parse(String.format("%d/%d/%d", month, day, year));
+    }
+
+    public Date getDate()
+    {
+        return this.date;
     }
 }
--- c09-inheritance-and-interfaces/business/P09_21/Demo.java
+++ c09-inheritance-and-interfaces/business/P09_21/Demo.java
@@ -1,8 +1,18 @@
+import java.util.Date;
+
 public class Demo
 {
     public static void main(String[] args)
     {
-        Appointment test = new Appointment("Hello World");
-        System.out.println(test.getDescription());
+        try
+        {
+            Appointment appt = new Appointment("Hello World", 9, 7, 2019);
+            System.out.println(appt.getDescription());
+            System.out.println(appt.getDate());
+        }
+        catch(Exception e)
+        {
+            System.out.println("Houston we have a problem");
+        }
     }
 }
