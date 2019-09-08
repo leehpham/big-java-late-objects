1a1832a7b0fa7e72ae3bcead36396b02a73af707
Hoang Pham <lehoangpham1092@gmail.com>
2019-09-07 21:23:08 -0500

    ...

--- c09-inheritance-and-interfaces/business/P09_21/Appointment.java
+++ c09-inheritance-and-interfaces/business/P09_21/Appointment.java
@@ -1,10 +1,16 @@
 public class Appointment
 {
     private String description;
+    private int year;
+    private int month;
+    private int day;
 
-    public Appointment(String description)
+    public Appointment(String description, int year, int month, int day)
     {
         this.description = description;
+        this.year = year;
+        this.month = month;
+        this.day = day;
     }
 
     public String getDescription()
