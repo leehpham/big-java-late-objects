c9a6755b55256c98413c23ca9dd64f28ba9cead8
Hoang Pham <lehoangpham1092@gmail.com>
2019-10-29 21:31:11 -0500

    Initial commit with C#

---
+++ c14-sorting-and-searching/14-01-selection-sort/Section01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Section01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = ArrayUtil.RandomIntArray(20, 100);
+            Console.WriteLine(string.Join(",", a));
+
+            SelectionSorter.Sort(a);
+
+            Console.WriteLine(string.Join(",", a));
+        }
+    }
+}
