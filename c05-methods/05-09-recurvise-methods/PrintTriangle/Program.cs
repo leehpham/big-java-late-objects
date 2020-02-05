cde0a68690bffdd0b65cc8d99c3926094cbe3574
Hoang Pham <lehoangpham1092@gmail.com>
2020-02-05 06:31:53 -0600

    ...

---
+++ c05-methods/05-09-recurvise-methods/PrintTriangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle(5);
+        }
+
+        public static void PrintTriangle(int sideLength)
+        {
+            if (sideLength < 1) { return; }
+            PrintTriangle(sideLength - 1);
+            for (int i = 0; i < sideLength; i++)
+            {
+                Console.Write("[]");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ c05-methods/05-09-recurvise-methods/ex/P05_16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P05_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("Hoang"));
+            Console.WriteLine(IsPalindrome("exxe"));
+            Console.WriteLine(IsPalindrome("rotor"));
+            Console.WriteLine(IsPalindrome("mm"));
+            Console.WriteLine(IsPalindrome("o"));
+            Console.WriteLine(IsPalindrome("mn"));
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            if (str.Length == 1 || (str.Length == 2 && str[0] == str[str.Length - 1]))
+            {
+                return true;
+            }
+            else if (str.Length == 2 && str[0] != str[str.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                var isPalindrome = IsPalindrome(str.Substring(1, str.Length -2)) && str[0] == str[str.Length - 1];
+                return isPalindrome;
+            }
+        }
+    }
+}
