07a1b1c1fc78252b47c76a7e044629259dc0958c
Hoang Pham <lehoangpham1092@gmail.com>
2021-05-14 15:27:28 -0500

    ...

---
+++ c14-sorting-and-searching/14-07-estimate-running-time-algorithm/exercises/P14_08/P14_08.java
@@ -0,0 +1,16 @@
+public class P14_08
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+    private static int findMostFrequentElement(int[] arr)
+    {
+        for (int num : arr)
+        {
+            System.out.println(num);
+        }
+        return 3;
+    }
+}
